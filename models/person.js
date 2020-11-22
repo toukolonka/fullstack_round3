8d3ab830347743566201a069916eae658eb228b5
Your Name <you@example.com>
2020-11-22 15:33:36 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+var uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URI;
 
@@ -18,9 +19,11 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, minlength: 3, required: true },
-  number: { type: String, minlength: 3, required: true },
+  name: { type: String, minlength: 3, required: true, unique: true },
+  number: { type: String, minlength: 8, required: true },
 });
+
+personSchema.plugin(uniqueValidator);
 
 personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
